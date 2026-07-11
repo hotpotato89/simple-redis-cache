442d4bafd26396576e6928fdaf476b11d4ccc898
hotpotato <ramilreading@gmail.com>
2026-07-11 20:22:33 +0300

    tests(conftest): async and sync clients

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,14 +1,26 @@
-from fakeredis.aioredis import FakeRedis
+from fakeredis.aioredis import FakeRedis as AsyncFakeRedis
+from fakeredis import FakeRedis as SyncFakeRedis
 import pytest
 
-from src.simple_redis_cache.asyncio.cache import Cache
+from src.simple_redis_cache.asyncio.cache import Cache as AsyncCache
+from src.simple_redis_cache.sync.cache import Cache as SyncCache
 
 
 @pytest.fixture()
-async def fakeredis_client() -> FakeRedis:
-    return FakeRedis()
+async def async_fakeredis_client() -> AsyncFakeRedis:
+    return AsyncFakeRedis()
 
 
 @pytest.fixture()
-async def fake_cache(fakeredis_client: FakeRedis) -> Cache:
-    return Cache(fakeredis_client)
+def sync_fakeredis_client() -> SyncFakeRedis:
+    return SyncFakeRedis()
+
+
+@pytest.fixture()
+async def fake_async_cache(async_fakeredis_client: AsyncFakeRedis) -> AsyncCache:
+    return AsyncCache(async_fakeredis_client)
+
+
+@pytest.fixture()
+def fake_sync_cache(async_fakeredis_client: SyncFakeRedis) -> SyncCache:
+    return SyncCache(async_fakeredis_client)
