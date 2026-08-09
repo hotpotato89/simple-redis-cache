7b4545d11b50b723b498182561b72421f7bdefe8
hotpotato89 <ramilreading@gmail.com>
2026-08-09 16:11:11 +0300

    feat(serializer): use data compression and docstrings

--- src/simple_redis_cache/serializer.py
+++ src/simple_redis_cache/serializer.py
@@ -17,13 +17,21 @@ class Serializer:
     COMPRESS_THRESHOLD = 1024
 
     @classmethod
-    def dumps(cls, value: Any, use_pickle: bool = False) -> bytes:
+    def dumps(
+        cls,
+        value: Any,
+        use_pickle: bool = False,
+        compress: bool = False,
+        compress_threshold: int = COMPRESS_THRESHOLD,
+    ) -> bytes:
         """
         Сериализует значение в байты.
 
         Args:
             value: Любое Python-значение.
             use_pickle: Использовать pickle вместо JSON.
+            compress: Сжимать данные через lz4
+            compress_threshold: Порог сжатия данныъ в битах
 
         Returns:
             Байтовое представление для сохранения в Redis.
@@ -32,9 +40,14 @@ class Serializer:
             return cls.NULL_MARKER
 
         if use_pickle:
-            return cls.PICKLE_PREFIX + pickle.dumps(value)
+            data = cls.PICKLE_PREFIX + pickle.dumps(value)
+        else:
+            data = json.dumps(value, cls=cls.JSON_ENCODER).encode("utf-8")
 
-        return json.dumps(value, cls=cls.JSON_ENCODER).encode("utf-8")
+        if compress and len(data) > compress_threshold:
+            return cls._compress_data(data)
+
+        return data
 
     @classmethod
     def loads(cls, data: bytes) -> Any:
@@ -53,6 +66,9 @@ class Serializer:
         if data == cls.NULL_MARKER:
             return None
 
+        if data.startswith(cls.LZ4_PREFIX):
+            data = cls._decompress_data(data)
+
         if data.startswith(cls.PICKLE_PREFIX):
             return pickle.loads(data[len(cls.PICKLE_PREFIX) :])
 
@@ -63,6 +79,7 @@ class Serializer:
 
     @classmethod
     def _compress_data(cls, data: bytes) -> bytes:
+        """Сжимает данные через lz4"""
         import lz4.frame
 
         compressed = lz4.frame.compress(data)
@@ -70,6 +87,7 @@ class Serializer:
 
     @classmethod
     def _decompress_data(cls, data: bytes) -> bytes:
+        """Распаковывает данные через lz4"""
         import lz4.frame
 
         return lz4.frame.decompress(data[len(cls.LZ4_PREFIX) :])
